ba2feb56801c4eaf42e0aa4658dbf9c90acbdaf1
Lovecry <lovecry@hotmail.com>
2015-06-03 18:01:33 +0200

    Arrival Finito!

--- StateMachine/Agent.h
+++ StateMachine/Agent.h
@@ -13,11 +13,15 @@ public:
 	float getMaxVelocity() const { return m_maxVelocity; }
 	void setMaxVelocity(float val) { m_maxVelocity = val; }
 
-	Vector2 getVelocity() const { return velocity; }
-	void setVelocity(Vector2 val) { velocity = val; }
+	Vector2 getVelocity() const { return m_velocity; }
+	void setVelocity(Vector2 val) { m_velocity = val; }
+
+	Vector2 getAcceleration() const { return m_acceleration; }
+	void setAcceleration(Vector2 val) { m_acceleration = val; }
 
 private :
 	Vector2 m_position;
-	Vector2 velocity;
+	Vector2 m_velocity;
+	Vector2 m_acceleration;
 	float m_maxVelocity;
 };

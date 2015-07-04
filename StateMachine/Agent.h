53fda9532dd37e8aa31e9b4ccdc59392ef54d059
Mattia <m.mattia.carollo@gmail.com>
2015-07-04 11:27:33 +0200

    ..

--- StateMachine/Agent.h
+++ StateMachine/Agent.h
@@ -42,7 +42,6 @@ public:
 	SMnamespace::Vector2 getAcceleration() const { return m_vAcceleration; }
 	void setAcceleration(SMnamespace::Vector2 val) { m_vAcceleration = val; }
 
-	//void SetAgentType(int value){ m_eAgentType = value; };
 	int GetAgentType() const { return m_eAgentType; };
 
 protected:
@@ -53,6 +52,5 @@ protected:
 	float m_fCircleRadius;
 	float m_fAngleChange;
 	float m_fMaxVelocity;
-
 	type m_eAgentType;
 };
